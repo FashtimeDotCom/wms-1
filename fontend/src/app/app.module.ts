032547ff97e4e47884ee4162d17905e9eeb00304
Alexander Beshmelnitsyn <alexander.beshmelnitsyn@spetsservice.com>
2018-03-30 12:21:34 +0700

    Форма авторизации

--- fontend/src/app/app.module.ts
+++ fontend/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatToolbarModule} from "@angular/material";
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { appRoutes} from "./routes";
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
